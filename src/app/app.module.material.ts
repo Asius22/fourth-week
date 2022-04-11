62c5f2ac8632c0b2f776343c77d729c0a3069fbf
Asius22 <ciro_2000@live.it>
2022-04-11 12:20:19 +0200

    first angular

---
+++ src/app/app.module.material.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {  MatIconModule } from '@angular/material/icon'
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+
+})
+export class MaterialModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './app.module.material';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
